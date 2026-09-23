8337e487ff5126da9bc8828c05d1b9d5aa5e42e8
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2026-09-23 15:03:43 +0200

    Fix multicast sending

--- tools/start_multicast_imaging.sh
+++ tools/start_multicast_imaging.sh
@@ -4,6 +4,5 @@ IMAGE=$2
 mkdir -p /run/cranix
 /usr/sbin/udp-sender --pid-file /run/cranix/multicast-imaging.pid \
 	--log /run/cranix/multicast-imaging.log \
-	--daemon-mode --daemon-mode \
 	--nokbd --interface ${INTERFACE} \
 	--file ${IMAGE} --min-receivers 1 --min-wait 5 --max-wait 10
